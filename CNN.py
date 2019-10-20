435d77614b9892b8ec1bb2e43582c63976957902
Kiki <pompom1396@gmail.com>
2019-10-20 09:33:29 +0200

    Cleaned up comments

--- CNN.py
+++ CNN.py
@@ -8,7 +8,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def run_cnn():
-    #mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
     inputData = CNNutils.LoadInputIMG("labels/labels.csv")
 
     # Python optimisation variables
@@ -17,64 +16,26 @@ def run_cnn():
     batch_size = 16
 
     # declare the training data placeholders
-    # input x - probably sth like 98 x 98 pixels??
-    x = tf.placeholder(tf.float32, [None, 98, 98, 3])        # 9604 is just 98*98, not regarding 3 color dimensions... -> 28812
-    # reshape the input data so that it is a 4D tensor.  The first value (-1) tells function to dynamically shape that
-    # dimension based on the amount of data passed to it.  The two middle dimensions are set to the image size (i.e. 98
-    # x 98).  The final dimension is 1 as there is only a single colour channel i.e. grayscale.  If this was RGB, this
-    # dimension would be 3
-    #x_shaped = tf.reshape(x, [-1, 98, 98, 3])
-    # now declare the output data placeholder - what is this going to be?
-    #y = tf.placeholder(tf.float32, [None, 10])
+    x = tf.placeholder(tf.float32, [None, 98, 98, 3])        # 98*98 plus 3 color dimensions... -> 28812
+    # output data placeholder
     y = tf.placeholder(tf.float32, [None, ])      #or can it be an integer??
 
     # create some convolutional layers
     layer1, s1 = create_new_conv_layer(x, 3, 15, [5, 5], [2, 2], 1, name='layer1')
     # input_data, num_input_channels, num_filters, filter_shape, pool_shape, stride, name
-    # what happens to the 3 color dimensions? Is the output really 15 channels, or 3*15?
-    # changed input_channels to 3 (RGB)
+    # input_channels is 3 because of RGB
     layer2, s2 = create_new_conv_layer(layer1, 15, 30, [5, 5], [2, 2], 2, name='layer2')
-    # and here?
-    # what am I actually returning with the s1,2,3..?
 
     # another convolution added:
     layer3, s3 = create_new_conv_layer(layer2, 30, 40, [5, 5], [1,1], 2, name='layer3')  # no pooling
 
 
 
-    # flatten the output ready for the fully connected output stage - after two layers of stride 2 pooling, we go
-    # from 28 x 28, to 14 x 14 to 7 x 7 x,y co-ordinates, but with 64 output channels.  To create the fully connected,
-    # "dense" layer, the new shape needs to be [-1, 7 x 7 x 64]
-    flattened = tf.reshape(layer3, [-1, 4 * 4 * 40])
-
-    # hopefully the result is really 4 x 4...
-
-# I don't really need this, do I..
-    """
-    # fully connected layer
-    # setup some weights and bias values for this layer, then activate with ReLU
-    wd1 = tf.Variable(tf.truncated_normal([4 * 4 * 30, 1000], stddev=0.03), name='wd1')
-    # how many nodes?
-    bd1 = tf.Variable(tf.truncated_normal([1000], stddev=0.01), name='bd1')
-    dense_layer1 = tf.matmul(flattened, wd1) + bd1
-    dense_layer1 = tf.nn.relu(dense_layer1)
-
-    # another layer with softmax activations
-    wd2 = tf.Variable(tf.truncated_normal([1000, 10], stddev=0.03), name='wd2')
-    bd2 = tf.Variable(tf.truncated_normal([10], stddev=0.01), name='bd2')
-    dense_layer2 = tf.matmul(dense_layer1, wd2) + bd2
-    y_ = tf.nn.softmax(dense_layer2)
-    """
-
-    #cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=dense_layer2, labels=y))
-    #maybe I don't need this??
-    #instead:
+    # prediction:
     y_pred = s3
     #y_pred = s1 + s2 + s3
-    #y_ = tf.Variable(y)
-    #error = tf.math.square(y - y_pred)
+    # option to add different sums (all/some layers)
     error = tf.pow((y - y_pred), 2)
-    # TypeError: must be real number, not Tensor
     err_mean = tf.sqrt(tf.reduce_mean(error))
     # add an optimiser
     optimiser = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(error)
@@ -83,20 +44,12 @@ def run_cnn():
     # define an accuracy assessment operation
     #correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
     #correct_prediction = tf.equal(y, y_pred)
-    rmse = tf.pow((y - y_pred), 2)
-    accuracy = tf.sqrt(tf.reduce_mean(rmse))
+    rmse = tf.pow((y - y_pred), 2)      # same as error
+    accuracy = tf.sqrt(tf.reduce_mean(rmse))    # same as err_mean
 
     # setup the initialisation operator
     init_op = tf.global_variables_initializer()
 
-    # setup recording variables
-    # add a summary to store the accuracy
-    """
-    tf.summary.scalar('accuracy', accuracy)
-
-    merged = tf.summary.merge_all()
-    writer = tf.summary.FileWriter('C:\\Users\\Andy\\PycharmProjects')
-    """
 
     with tf.Session() as sess:
         # initialise the variables
@@ -109,19 +62,16 @@ def run_cnn():
                 batch_x, batch_y = inputData.train.next_batch(batch_size=batch_size)
                 _, c = sess.run([optimiser, err_mean], feed_dict={x: batch_x, y: batch_y})
 
-                # changed cross_entropy to error
                 avg_cost += c / total_batch
             test_acc, y_solv, y_pred_solv = sess.run([accuracy, y, y_pred], feed_dict={x: inputData.test.images, y: inputData.test.labels})
             #print("Epoch:", (epoch + 1), "cost =", "{:.3f}".format(avg_cost), " test accuracy:", "{:.3f}".format(test_acc))
             print("Epoch: ", str((epoch + 1)), "cost = ", str( avg_cost), "test accuracy: ", str(test_acc))
             print(list(zip(y_solv,y_pred_solv))[0:20])
 
-            if epoch % 20 == 0:
+            #if epoch % 20 == 0:
                 #LP: store trained filters into an image
-                getActivations(sess, layer1, inputData.test.images[4:8], x, epoch, "layer1")
-                getActivations(sess, layer2, inputData.test.images[4:8], x, epoch, "layer2")
-
-
+                #getActivations(sess, layer1, inputData.test.images[4:8], x, epoch, "layer1")
+                #getActivations(sess, layer2, inputData.test.images[4:8], x, epoch, "layer2")
 
             #print(y_solv)
             #print(y_pred_solv)
@@ -136,7 +86,6 @@ def run_cnn():
 
         #writer.add_graph(sess.graph)
         print(sess.run(accuracy, feed_dict={x: inputData.test.images, y: inputData.test.labels}))
-# changed all uses of mnist to inputData - will have to make same functionalitu for loading dats
 
 
 def getActivations(sess, layer, stimuli, x, iter, labelname):
@@ -202,20 +151,16 @@ def create_new_conv_layer(input_data, num_input_channels, num_filters, filter_sh
     #decreased SD
     weights = tf.Variable(tf.truncated_normal(conv_filt_shape, stddev=0.001), name=name+'_W')
     bias = tf.Variable(tf.truncated_normal([num_filters]), name=name+'_b')
-    # left the same as in tutorial - do I need to change it?
 
     # setup the convolutional layer operation
     out_layer = tf.nn.conv2d(input_data, weights, [1, stride, stride, 1], padding='VALID')
-    # changed 1 in brackets with strides to 3 - channels?
-    #changed padding - was that right??
-
     # add the bias
     out_layer += bias
 
-
     #LP: pred sectenim vystupu jsem je nechal prolozit pomoci sigmoidy - tedy da se to chapat jako pravdepodobnost,
     # ze dany filter zachytil label. Zatim nevim nakolik je tahle uprava rozumna, ale vypada to, ze to vcelku funguje
-    sigmoid_layer = tf.nn.sigmoid(out_layer)
+    sigmoid_layer = tf.nn.sigmoid(out_layer) #prenasobit 1.1, odecist 0.1, pak na to aplikovat relu
+    # ale nepouziva se pro vystup, jen pro sumu..?
 
     # apply a ReLU non-linear activation
     out_layer = tf.nn.relu(out_layer)
@@ -225,15 +170,8 @@ def create_new_conv_layer(input_data, num_input_channels, num_filters, filter_sh
     # - tady je potreba zachovat prvni dimenzi, jinak se to snazi odhadnout velikost cele batche
     sum_ = tf.reduce_sum(sigmoid_layer, axis=[1, 2, 3])
 
-    # now perform max pooling
-    # ksize is the argument which defines the size of the max pooling window (i.e. the area over which the maximum is
-    # calculated).  It must be 4D to match the convolution - in this case, for each image we want to use a 2 x 2 area
-    # applied to each channel
+    # now perform max pooling - ksize is window size
     ksize = [1, pool_shape[0], pool_shape[1], 1]
-    # strides defines how the max pooling area moves through the image - a stride of 2 in the x direction will lead to
-    # max pooling areas starting at x=0, x=2, x=4 etc. through your image.  If the stride is 1, we will get max pooling
-    # overlapping previous max pooling areas (and no reduction in the number of parameters).  In this case, we want
-    # to do strides of 2 in the x and y directions.
     strides = [1, 2, 2, 1]
     out_layer = tf.nn.max_pool(out_layer, ksize=ksize, strides=strides, padding='SAME')
     #here I left the padding, not sure why..
