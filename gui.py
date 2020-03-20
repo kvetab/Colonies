c98b3b249f8b466c04ef58397badca4292d2734f
Kiki <pompom1396@gmail.com>
2020-03-20 20:20:34 +0100

    Does not display prediction

--- gui.py
+++ gui.py
@@ -9,8 +9,8 @@ DEFAULT_MODEL = "models/model1584474209.720228"
 
 window = Tk()
 window.title("ColonyCount")
-window.geometry('350x500')
-# MODEL = PredictorKeras(DEFAULT_MODEL)
+window.geometry('400x200')
+MODEL = PredictorKeras(DEFAULT_MODEL)
 MODEL_DIR = DEFAULT_MODEL
 
 def choose_model(event):
@@ -24,6 +24,7 @@ def choose_photo(event):
     img_file = askopenfilename(initialdir=os.path.join(os.getcwd(), "photos_used"), title='Choose an image.')
     img = Image.open(img_file)
     win2 = Toplevel(window)
+    win2.geometry('600x400')
 
     frame = Frame(win2, bd=2, relief=SUNKEN)
     frame.grid_rowconfigure(0, weight=1)
@@ -36,26 +37,23 @@ def choose_photo(event):
     canvas.grid(row=0, column=0, sticky=N + S + E + W)
     xscroll.config(command=canvas.xview)
     yscroll.config(command=canvas.yview)
-    frame.pack(fill=BOTH, expand=1)
+    frame.pack(fill=BOTH, expand=0)
 
     #cv = Canvas(win2)
     photo = ImageTk.PhotoImage(img)
     #cv.grid(column=0, row=0)
     canvas.create_image(0, 0, image=photo, anchor='nw')
     canvas.config(scrollregion=canvas.bbox(ALL))
+
+    prediction, positive = MODEL.predict(img_file, verbose=0)
+    prediction_label = Label(win2, text="Prediction is: " + str(prediction))
+    prediction_label.grid_rowconfigure(1)
+    prediction_label_pos = Label(win2, text="Prediction is: " + str(positive))
+    prediction_label_pos.grid_rowconfigure(2)
+
+
     win2.mainloop()
 
-    """
-    img_file = askopenfilename(initialdir=os.path.join(os.getcwd(), "photos_used"), title='Choose an image.')
-    win2 = Toplevel(window)
-    cv = Canvas(win2)
-    #frame.pack(fill=BOTH, expand=1)
-    #img_file = 'photos_used/PICT9575.png'
-    img = Image.open(img_file)
-    photo = ImageTk.PhotoImage(img_file)
-    cv.grid(column=0, row=3)
-    cv.create_image(50, 50, image=photo, anchor='nw')
-    """
 
 
 label_model = Label(window, text="Current model: ")
